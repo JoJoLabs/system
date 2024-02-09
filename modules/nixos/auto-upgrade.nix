dd2e333cb0d06475bbe4420921b5cc246600924a
Joris Bolsens <joris@jojolabs.com>
2024-02-09 13:10:15 -0800

    maybe we dont like _

--- modules/nixos/auto-upgrade.nix
+++ modules/nixos/auto-upgrade.nix
@@ -1,11 +1,21 @@
-{ flakeURI ? "github:jojolabs/system#joris@linux", ... }: {
-  system.autoUpgrade = {
-    enable = true;
-    flags = ["--update-input" "nixpkgs"];
-    flake = toString flakeURI;
-    dates = "hourly";
-    allowReboot = true;
-    randomizedDelaySec = "45min";
-    rebootWindow = { lower = "01:00"; upper = "05:00"; };
+{ ... }: {
+
+  options = {
+    flakeURI = mkOption {
+      type = types.string;
+      default = "github:jojolabs/system#joris@linux";
+    };
+  };
+
+  config = {
+    system.autoUpgrade = {
+      enable = true;
+      flags = ["--update-input" "nixpkgs"];
+      flake = config.flakeURI;
+      dates = "hourly";
+      allowReboot = true;
+      randomizedDelaySec = "45min";
+      rebootWindow = { lower = "01:00"; upper = "05:00"; };
+    };
   };
 }
