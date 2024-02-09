c64796e2a44e7f66068be5e2a31ff7ab795134d2
Joris Bolsens <joris@jojolabs.com>
2024-02-08 22:43:31 -0800

    add git to installer

--- modules/nixos/installer.nix
+++ modules/nixos/installer.nix
@@ -14,6 +14,12 @@
   # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   # sops.secrets.github_private_key = {};
 
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+  ];
+
   systemd.services.inception = {
     description = "Self-bootstrap a NixOS installation";
     wantedBy = [ "multi-user.target" ];
@@ -21,8 +27,9 @@
     path = [ "/run/current-system/sw/" ];
     script = with pkgs; ''
       sleep 60
+      ${config.system.build.nixos-install}/bin/nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ${toString self}/modules/disko/disko-config.nix
       mkdir -p /mnt/etc/nixos/   
-      ${config.system.build.nixos-install}/bin/nixos-install -j 4 --flake git+ssh://git@github.com/JoJoLabs/system#joris@x86_64-linux --no-root-passwd
+      ${config.system.build.nixos-install}/bin/nixos-install --experimental-features "nix-command flakes" -j 4 --flake git+ssh://git@github.com/JoJoLabs/system#joris@x86_64-linux --no-root-passwd
       ${systemd}/bin/shutdown -r now
     '';
     environment = config.nix.envVars // {
