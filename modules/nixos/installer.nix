994e3f5f128d8a2741b6315f072782b78f08fdc8
Joris Bolsens <joris@jojolabs.com>
2024-02-08 22:50:29 -0800

    add git to installer

--- modules/nixos/installer.nix
+++ modules/nixos/installer.nix
@@ -27,7 +27,7 @@
     path = [ "/run/current-system/sw/" ];
     script = with pkgs; ''
       sleep 60
-      ${config.system.build.nixos-install}/bin/nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ${toString self}/modules/disko/disko-config.nix
+      ${config.system.build.nix}/bin/nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ${toString self}/modules/disko/disko-config.nix
       mkdir -p /mnt/etc/nixos/   
       ${config.system.build.nixos-install}/bin/nixos-install --experimental-features "nix-command flakes" -j 4 --flake git+ssh://git@github.com/JoJoLabs/system#joris@x86_64-linux --no-root-passwd
       ${systemd}/bin/shutdown -r now
