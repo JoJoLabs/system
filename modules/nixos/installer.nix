5626f9b05ec5329d470804c08bcab8428b39c793
Joris Bolsens <joris@jojolabs.com>
2024-02-08 23:17:27 -0800

    fix disko

--- modules/nixos/installer.nix
+++ modules/nixos/installer.nix
@@ -20,23 +20,14 @@
     git
   ];
 
-  tools = lib.makeBinPath (
-    with pkgs; [
-      config.system.build.nixos-enter
-      config.system.build.nixos-install
-      nix
-      parted
-    ]
-  );
-
   systemd.services.inception = {
     description = "Self-bootstrap a NixOS installation";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" "polkit.service" ];
-    path = [ "${tools}" ];
+    path = [ "/run/current-system/sw/" ];
     script = with pkgs; ''
       sleep 60
-      ${config.system.build.nix}/bin/nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ${toString self}/modules/disko/disko-config.nix
+      ${nix}/bin/nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ${toString self}/modules/disko/default.nix
       mkdir -p /mnt/etc/nixos/   
       ${config.system.build.nixos-install}/bin/nixos-install --experimental-features "nix-command flakes" -j 4 --flake git+ssh://git@github.com/JoJoLabs/system#joris@x86_64-linux --no-root-passwd
       ${systemd}/bin/shutdown -r now
