02f9122aafd4b40794155605e18b10ae8f0798db
Joris Bolsens <joris@jojolabs.com>
2024-02-09 23:50:02 -0800

    add alias

--- flake.nix
+++ flake.nix
@@ -212,7 +212,7 @@
           disko.nixosModules.disko
           sops-nix.nixosModules.sops
           ./modules/disko/default.nix
-          ./modules/nixos/common.nix
+          ./modules/nixos/alias.nix
           ./profiles/kubemaster.nix
           ./modules/nixos/installer.nix
         ];
