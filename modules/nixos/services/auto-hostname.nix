55a3b1c4596c2456a2f2f41e79e30c920ca4972a
Joris Bolsens <joris@jojolabs.com>
2024-02-18 16:35:15 -0800

    set transient hostname

--- modules/nixos/services/auto-hostname.nix
+++ modules/nixos/services/auto-hostname.nix
@@ -10,7 +10,7 @@
     path = [ "/run/current-system/sw/" ];
     script = with pkgs; ''
       uuid=$(${pkgs.dmidecode}/bin/dmidecode -- -s system-uuid | base64 | head -c 8 | tr '[:upper:]' '[:lower:]')
-      hostnamectl set-hostname nixos-$uuid
+      hostnamectl set-hostname nixos-$uuid --transient
     '';
     serviceConfig = {
       Type = "oneshot";
