1683114f54661ee3dcef28ea69a04182bd818299
Joris Bolsens <joris@jojolabs.com>
2024-03-03 15:13:53 -0800

    {} -> []

--- modules/nixos/kubernetes/node.nix
+++ modules/nixos/kubernetes/node.nix
@@ -24,11 +24,11 @@ in
       2601
       22
     ];
-    trustedInterfaces = {
+    trustedInterfaces = [
       "enp1s0"
       "flannel.1"
       "mynet"
-    };
+    ];
   };
 
   services.kubernetes = let
