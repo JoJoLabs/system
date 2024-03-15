66b1bba66aa1202db973b720a6f285eaa7b5ea88
Joris Bolsens <joris@jojolabs.com>
2024-03-15 16:01:10 -0700

    add drbd

--- modules/nixos/kubernetes/drbd.nix
+++ modules/nixos/kubernetes/drbd.nix
@@ -1,10 +1,10 @@
 { config, self, pkgs, lib, ... }: {
   boot.extraModulePackages = [ pkgs.drbd-mod ];
-  services.drbd.enable = true;
-  services.drbd.enable_helper = false;
+  
   boot.extraModprobeConfig = ''
     options drbd usermode_helper=disabled
   '';
+  boot.kernelModules = [ "drbd" ];
   # environment.etc."modprobe.d/drbd.conf".source = pkgs.writeTextFile { 
   #   name = "drbd.conf";
   #   text = "options drbd usermode_helper=disabled";
