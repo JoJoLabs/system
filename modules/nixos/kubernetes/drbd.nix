3862f14d91b538d34b843ebcec3972da43047969
Joris Bolsens <joris@jojolabs.com>
2024-03-21 12:15:16 -0700

    lock

--- modules/nixos/kubernetes/drbd.nix
+++ modules/nixos/kubernetes/drbd.nix
@@ -5,10 +5,7 @@
     options drbd usermode_helper=disabled
   '';
   boot.kernelModules = [ "drbd" ];
-  # environment.etc."modprobe.d/drbd.conf".source = pkgs.writeTextFile { 
-  #   name = "drbd.conf";
-  #   text = "options drbd usermode_helper=disabled";
-  # };
+  
   systemd.tmpfiles.rules = [
     "d /usr/src 0755 root root -"
   ];
